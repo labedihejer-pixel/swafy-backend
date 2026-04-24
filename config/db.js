6e3a25f285694299ad40dd59913493f7925f7e8a
labedihejer-pixel <labedihejer@gmail.com>
2026-04-24 18:42:43 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),   // ⭐ المهم
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  port: Number(process.env.DB_PORT),
 });
 
 pool.getConnection()
