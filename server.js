f9e4a4163d6f1bcfaf195a334660b733347737ca
labedihejer-pixel <labedihejer@gmail.com>
2026-04-24 12:00:10 +0100

    fix CORS for frontend

--- server.js
+++ server.js
@@ -35,15 +35,9 @@ const ALLOWED_ORIGINS = [
 // ===============================
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || ALLOWED_ORIGINS.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
