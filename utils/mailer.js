745b3c1d37214a2756cbc14ac2cc26ddc2e5bcf3
labedihejer-pixel <labedihejer@gmail.com>
2026-05-17 07:29:30 +0100

    mailtrap working

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,48 +1,27 @@
-const https = require('https');
+const nodemailer = require("nodemailer");
 
-const sendEmail = async (toEmail, subject, htmlContent) => {
-  console.log('🔑 API KEY:', process.env.BREVO_API_KEY?.substring(0, 10) + '...');
-  const data = JSON.stringify({
-  sender: { name: "Swafy", email: process.env.SENDER_EMAIL },
-  to: [{ email: toEmail }],
-  subject: subject,
-  htmlContent: htmlContent,
-
-  headers: {
-    "X-Mailin-custom": "SwafyApp"
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "430a513c20788f",     // من Mailtrap
+    pass: "6ee5afe2c861c3" // حط password الحقيقي
   }
-
 });
 
-
-  return new Promise((resolve, reject) => {
-    const req = https.request({
-      hostname: 'api.brevo.com',
-      path: '/v3/smtp/email',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'api-key': process.env.BREVO_API_KEY,
-        'Content-Length': Buffer.byteLength(data)
-      }
-    }, (res) => {
-      let body = '';
-      res.on('data', chunk => body += chunk);
-      res.on('end', () => {
-        if (res.statusCode === 201) {
-          console.log('✅ Email envoyé avec succès');
-          resolve(body);
-        } else {
-          console.error('❌ Erreur Brevo:', body);
-          reject(new Error(`Status: ${res.statusCode} - Body: ${body}`));
-        }
-      });
+const sendEmail = async (toEmail, subject, htmlContent) => {
+  try {
+    await transporter.sendMail({
+      from: "swafy@test.com",
+      to: toEmail,
+      subject: subject,
+      html: htmlContent
     });
 
-    req.on('error', reject);
-    req.write(data);
-    req.end();
-  });
+    console.log(" Email envoyé via Mailtrap");
+  } catch (err) {
+    console.error("❌ Mailtrap error:", err);
+  }
 };
 
 module.exports = { sendEmail };
